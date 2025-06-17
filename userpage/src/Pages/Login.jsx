332725b977cde6d4fab0b0e41670e87fb29ecfd7
Kailas <kailas1246@gmail.com>
2025-06-17 19:34:29 +0530

    Initial commit

---
+++ userpage/src/Pages/Login.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useNotification } from "../Components/NotificationProvider";
+
+export default function Login() {
+    const { addNotification } = useNotification();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSignup = async () => {
+        navigate('/signup')
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+
+        try {
+            const res = await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL}/api/auth/login`, { email, password });
+
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('user', JSON.stringify(res.data.user));
+
+            navigate('/add-product');
+
+            addNotification({
+                type: 'success',
+                message: 'Login Successfully!',
+            });
+
+        } catch (err) {
+            setError(err.response?.data?.message || 'Login failed');
+            addNotification({
+                type: 'faild',
+                message: 'Login Failed!',
+            });
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-50">
+            <form onSubmit={handleSubmit} className="bg-white p-8 rounded shadow-md w-full max-w-md">
+                <h2 className="text-2xl font-bold mb-6 text-center">Admin Login</h2>
+                {error && <p className="text-red-500 mb-4">{error}</p>}
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full p-3 border rounded mb-4"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="w-full p-3 border rounded mb-6"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button
+                    type="submit"
+                    className="w-full bg-indigo-600 hover:bg-indigo-700 text-white py-3 rounded font-semibold transition"
+                >
+                    Login
+                </button>
+                <div className="mt-4 text-center text-sm text-gray-500">
+                    Don’t have an account?
+                    <button
+                        onClick={handleSignup}
+                        className="ml-1 text-indigo-600 hover:underline font-medium"
+                    >
+                        Sign up
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    );
+}
